0cfeedecbfdd4ff91809b3c658408b5d6f975574
SumeyyeByrktr <bayraktar.smyy18@gmail.com>
2022-11-06 01:12:35 +0300

    Add project files.

---
+++ Dene1/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dene1.Areas.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
---
+++ Dene1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Dene1.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Dene1.Areas.Identity.IdentityHostingStartup))]
+namespace Dene1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Dene1DBContext>(options =>
+                    options.UseNpgsql(
+                        context.Configuration.GetConnectionString("LoginDb")));
+
+                services.AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                   
+                    })
+                    .AddEntityFrameworkStores<Dene1DBContext>();
+            });
+        }
+    }
+}
